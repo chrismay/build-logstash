0cd119e99fe546d9d571333f3044f35a513ab054
cusaab <cusaab@liathach.warwick.ac.uk>
2011-02-26 21:29:03 +0000

    add es building

--- build-logstash.pp
+++ build-logstash.pp
@@ -33,20 +33,34 @@ class elasticsearch-build{
    package{["openjdk-6-jdk"]: ensure=>present}
    file{"/opt": ensure=>directory}
 
-   exec{"/usr/bin/git clone https://github.com/elasticsearch/elasticsearch.git elasticsearch-src":
+   exec{"/usr/bin/git clone https://github.com/elasticsearch/elasticsearch.git elasticsearch-src && cd elasticsearch-src && /usr/bin/git checkout v${es_version}":
      cwd=>"/opt",
      creates=>"/opt/elasticsearch-src",
      require=>[File["/opt"],Package["git-core"]],
      alias=>clone-elasticsearch
    }
-   exec{"/opt/elasticsearch-src/gradlew":
-      require=>Exec["clone-elasticsearch"]
-      ,creates=>"/opt/elasticsearch-src/build/distributions"
+   exec{"/opt/elasticsearch-src/gradlew > /tmp/elasticsearch-gradle.out 2>&1":
+       cwd=>"/opt/elasticsearch-src",
+      require=>Exec["clone-elasticsearch"],
+      creates=>"/opt/elasticsearch-src/build/distributions/elasticsearch-${es_version}.tar.gz",
+      alias=>"build-elasticsearch"
    }
-  file{["/opt/elasticsearch-src/package","/opt/elasticsearch-src/package/DEBIAN"]: 
+  file{["/opt/elasticsearch-src/package","/opt/elasticsearch-src/package/DEBIAN", "/opt/elasticsearch-src/package/opt"]: 
      ensure=>directory,
      require=>Exec[clone-elasticsearch]
   }
+  exec {"/bin/tar zxf /opt/elasticsearch-src/build/distributions/elasticsearch-${es_version}.tar.gz && mv elasticsearch-${es_version} opt/elasticsearch":
+     cwd=>"/opt/elasticsearch-src/package",
+     creates=>"/opt/elasticsearch-src/package/opt/elasticsearch", 
+     require=>[Exec["build-elasticsearch"],File["/opt/elasticsearch-src/package/opt"]],
+     alias=>unpack-elasticsearch-tarball
+  }
+  exec{"/usr/bin/dpkg --build package/ elasticsearch-${es_version}_all.deb && cp elasticsearch-${es_version}_all.deb /vagrant":
+     cwd=>"/opt/elasticsearch-src",
+     creates=>"/opt/elasticsearch-src/elasticsearch-${es_version}_all.deb",
+     require=>Exec["unpack-elasticsearch-tarball"]
+  }
+  
   file{"/opt/elasticsearch-src/package/DEBIAN/control": 
      source=>"/vagrant/elasticsearch/package/DEBIAN/control",
      require=>Exec[clone-elasticsearch]
@@ -63,4 +77,5 @@ file{"/etc/apt/apt.conf.d/01proxy":
   content=>"Acquire::http { Proxy \"http://liathach:3142\"; };"
 }
 include grok-build
+$es_version="0.15.0"
 include elasticsearch-build
